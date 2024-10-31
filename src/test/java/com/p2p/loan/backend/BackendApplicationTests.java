d39348d3d3a8559b940281ee0cb4453898dfcdda
chenqilin70 <kylinchen85@foxmail.com>
2024-10-31 09:26:34 +0800

    first commit

---
+++ src/test/java/com/p2p/loan/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2p.loan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
