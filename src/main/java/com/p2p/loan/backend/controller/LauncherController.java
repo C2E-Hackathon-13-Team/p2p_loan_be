6434ba00e29b9d2946434c3fd5746417c53d64b3
Kylin <879646473@qq.com>
2024-11-02 23:08:22 +0800

    update

--- src/main/java/com/p2p/loan/backend/controller/LauncherController.java
+++ src/main/java/com/p2p/loan/backend/controller/LauncherController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("launcher")
 @Tag(name = "筹资人")
+@CrossOrigin(origins = "*")
 @Slf4j
 public class LauncherController {
 
